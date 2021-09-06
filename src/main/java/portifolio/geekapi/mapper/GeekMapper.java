7434d5fb9bd256622189346b5eef8ae31740a1e1
Dan <dantraverzim@gmail.com>
2021-09-06 00:02:20 -0300

    remoção de comentários

--- src/main/java/portifolio/geekapi/mapper/GeekMapper.java
+++ src/main/java/portifolio/geekapi/mapper/GeekMapper.java
@@ -4,7 +4,6 @@ import portifolio.geekapi.dto.request.GeekDTO;
 import portifolio.geekapi.entity.Geek;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-//import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel="spring")
 public interface GeekMapper {
